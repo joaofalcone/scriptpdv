1992c6ef97b7bf346ff243bd83e88c4af9f69961
joaofalcone <61852799+joaofalcone@users.noreply.github.com>
2026-05-15 13:37:20 -0300

    Update and rename executa_tls.sh to instalar_tls.sh

---
+++ instalar_tls.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+echo "Atualizando repositórios e instalando dependências..."
+sudo apt update && sudo apt install -y mysql-client sqlite3 zenity
+
+URL_SCRIPT="https://raw.githubusercontent.com/joaofalcone/scriptpdv/main/tls.sh"
+TMP_SCRIPT="/tmp/tls.sh"
+
+echo "Baixando tls.sh..."
+wget -qO "$TMP_SCRIPT" "$URL_SCRIPT"
+
+chmod +x "$TMP_SCRIPT"
+
+echo "Executando tls.sh..."
+bash "$TMP_SCRIPT"
